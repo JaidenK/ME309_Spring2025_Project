23f15b9de55f1b49f74a206cd2386d0c1025f870
JaidenK <king.jaiden@yahoo.com>
2025-03-12 23:01:23 -0700

    Data processing algorithm to remove frozen frames of data

--- TrajEst_TopScript.m
+++ TrajEst_TopScript.m
@@ -13,6 +13,51 @@ data = readmatrix(input_file);
 % - Remove duplicate frames. Essentially a low pass filter? Cull outliers?
 % - Iteratively cull outliers until minimium number of fit points perfectly
 % define the plot.
+figure(); 
+ax1 = subplot(4,1,1);
+xy_diff = diff(data(:,[2 3]));
+plot(xy_diff(:,1),'DisplayName','diff(x)'); 
+hold on; 
+plot(xy_diff(:,2),'DisplayName','diff(y)');
+xy_means = movmean(xy_diff,5,1);
+plot(xy_means);
+title("Data Processing Algo: Detecting frozen frames");
+ylabel("element-wise difference (meters)");
+legend;
+ax2 = subplot(4,1,2);
+xy_highpass = xy_means-xy_diff;
+plot(xy_highpass);
+hold on;
+ax3 = subplot(4,1,3);
+kernel = [-.25 -.25 1];
+outliers = zeros(size(xy_highpass));
+for i=(length(kernel)):size(outliers,1)
+    rows = (i-length(kernel)+1):i;
+    thresh = .02;
+    outliers(i,1) = all((xy_highpass(rows,1).*kernel')>0) && (max(xy_highpass(rows,1)) > thresh);
+    outliers(i,2) = all((xy_highpass(rows,2).*kernel')>0) && (max(xy_highpass(rows,2)) > thresh);
+end
+plot(outliers);
+outliers = outliers(:,1) | outliers(:,2);
+hold on;
+iOutliers = find(outliers)
+iiOutliers = zeros(size(iOutliers))
+for i = 1:length(iiOutliers)
+    index = (length(kernel)*(i-1));
+    iiOutliers(index+1) = iOutliers(i)-0;
+    iiOutliers(index+2) = iOutliers(i)-1;
+    iiOutliers(index+3) = iOutliers(i)-2;
+end
+ax4 = subplot(4,1,4);
+% Remove the data
+data(iiOutliers,:) = [];
+xy_diff = diff(data(:,[2 3]));
+plot(xy_diff(:,1)); 
+hold on;
+plot(xy_diff(:,2)); 
+
+linkaxes([ax1 ax2 ax3 ax4],'x');
+
 % Remove all rows containing NaN
 data(any(isnan(data), 2), :) = []; % https://www.mathworks.com/matlabcentral/answers/31971-delete-rows-with-nan-records
 t_sampled = data(:,1);
@@ -24,6 +69,7 @@ if(size(data,2)>3)
     y2_sampled = data(:,5);
     hasChuteData = true;
 end
+
 
 % Plot raw data to confirm we're on the right track
 hFig_raw = figure();
