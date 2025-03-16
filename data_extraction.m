94e72ce62836151eb61144053e3557f790bda880
JaidenK <king.jaiden@yahoo.com>
2025-03-15 20:43:01 -0700

    Remove rebound from data

--- data_extraction.m
+++ data_extraction.m
@@ -1,14 +1,28 @@
 function [t_sampled,x_sampled,y_sampled,x2_sampled,y2_sampled,hasChuteData] = data_extraction(data)
 %DATA_EXTRACTION Summary of this function goes here
 %   Detailed explanation goes here
-t_sampled = data(:,1);
-x_sampled = data(:,2);
+
+
 y_sampled = data(:,3);
+
+% Find the impact index and cut off the rebound for simplicity.
+% Assume the lowest y position is impact. This isn't true if it bounces a
+% second time, closer to the camera
+iImpact = find(y_sampled == min(y_sampled));
+
+t_sampled = data(1:iImpact,1);
+x_sampled = data(1:iImpact,2);
+y_sampled = data(1:iImpact,3);
 hasChuteData = false;
+x2_sampled = zeros(size(x_sampled));
+y2_sampled = zeros(size(y_sampled));
 if(size(data,2)>3)
-    x2_sampled = data(:,4);
-    y2_sampled = data(:,5);
+    x2_sampled = data(1:iImpact,4);
+    y2_sampled = data(1:iImpact,5);
     hasChuteData = true;
 end
+
+
+
 end
 
