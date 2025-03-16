53bbac60522cf14a6f5f8ae1dfb5647be6b20488
JaidenK <king.jaiden@yahoo.com>
2025-03-15 21:17:32 -0700

    Outlier removal improvement

--- data_processing.m
+++ data_processing.m
@@ -10,45 +10,52 @@ function [data] = data_processing(data)
 figure(); 
 ax1 = subplot(4,1,1);
 xy_diff = diff(data(:,[2 3]));
-plot(xy_diff(:,1),'DisplayName','diff(x)'); 
+t = data(:,1);
+t = t(1:end-1);
+plot(t,xy_diff(:,1),'DisplayName','diff(x)'); 
 hold on; 
-plot(xy_diff(:,2),'DisplayName','diff(y)');
-xy_means = movmean(xy_diff,5,1);
-plot(xy_means);
+plot(t,xy_diff(:,2),'DisplayName','diff(y)');
+%xy_means = movmean(xy_diff,5,1);
+%plot(xy_means);
 title("Data Processing Algo: Detecting frozen frames");
 ylabel("element-wise difference (meters)");
 legend;
+
 ax2 = subplot(4,1,2);
-xy_highpass = xy_means-xy_diff;
-plot(xy_highpass);
-hold on;
+%xy_highpass = xy_means-xy_diff;
+%plot(xy_highpass);
+%ylabel("High pass");
+%hold on;
+
 ax3 = subplot(4,1,3);
-kernel = [-.25 -.25 1];
-outliers = zeros(size(xy_highpass));
-for i=(length(kernel)):size(outliers,1)
-    rows = (i-length(kernel)+1):i;
-    thresh = .02;
-    outliers(i,1) = all((xy_highpass(rows,1).*kernel')>0) && (max(xy_highpass(rows,1)) > thresh);
-    outliers(i,2) = all((xy_highpass(rows,2).*kernel')>0) && (max(xy_highpass(rows,2)) > thresh);
+iOutliers = [];
+for i = 1:length(xy_diff(:,1))
+    if(any(xy_diff(i,:)==0))
+        iOutliers(end+1) = i+1;
+    end
 end
-plot(outliers);
-outliers = outliers(:,1) | outliers(:,2);
-hold on;
-iOutliers = find(outliers);
-iiOutliers = zeros(size(iOutliers).*[length(kernel) 1]);
-for i = 1:length(iOutliers)
-    index = (length(kernel)*(i-1));
-    iiOutliers(index+1) = iOutliers(i)-0;
-    iiOutliers(index+2) = iOutliers(i)-1;
-    iiOutliers(index+3) = iOutliers(i)-2;
-end
+%disp(iOutliers);
+
+%outliers = outliers(:,1) | outliers(:,2);
+%ylabel("outliers")
+%hold on;
+%iOutliers = find(outliers);
+%iiOutliers = zeros(size(iOutliers).*[length(kernel) 1]);
+%for i = 1:length(iOutliers)
+%    index = (length(kernel)*(i-1));
+%    iiOutliers(index+1) = iOutliers(i)-0;
+%    iiOutliers(index+2) = iOutliers(i)-1;
+%    iiOutliers(index+3) = iOutliers(i)-2;
+%end
+
 ax4 = subplot(4,1,4);
 % Remove the data
-data(iiOutliers,:) = [];
+data(iOutliers,:) = [];
 xy_diff = diff(data(:,[2 3]));
-plot(xy_diff(:,1)./diff(data(:,1))); 
+t = data(1:end-1,1);
+plot(t,xy_diff(:,1)./diff(data(:,1))); 
 hold on;
-plot(xy_diff(:,2)./diff(data(:,1))); 
+plot(t,xy_diff(:,2)./diff(data(:,1))); 
 
 linkaxes([ax1 ax2 ax3 ax4],'x');
 
