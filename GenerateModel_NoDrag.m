4521895d650e12fbf226ad53553c79e1cbf3f69b
JaidenK <king.jaiden@yahoo.com>
2025-03-20 20:33:28 -0700

    Bug fix

--- GenerateModel_NoDrag.m
+++ GenerateModel_NoDrag.m
@@ -2,12 +2,12 @@ function [Model] = GenerateModel_NoDrag(ModelParameters)
 Model = struct();
 
 % Decode parameters
-g = ModelParameters.g;
+g  = ModelParameters.g;
 r0 = ModelParameters.r0;
 v0 = ModelParameters.v0;
 
 % implement equations that were derived in the notes.
 Model.a_of_t = @(t) [zeros(size(t)); -g*ones(size(t))];
-Model.v_of_t = @(t) [v0(1); v0(2)-g*t];
-Model.r_of_t = @(t) [v0(1)*t + r0(1); -(1/2)*g*t.^2];
+Model.v_of_t = @(t) [v0(1)*ones(size(t)); v0(2)-g*t];
+Model.r_of_t = @(t) [v0(1)*t + r0(1); -(1/2)*g*t.^2 + v0(2)*t + r0(2)];
 end
