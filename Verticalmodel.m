69030e8a21754f9996c0f5504ad7a3cc7fc64fe7
Rescuerich <richard.rojas.717@my.csun.edu>
2025-04-02 12:37:09 -0700

    added plot of position

--- Verticalmodel.m
+++ Verticalmodel.m
@@ -1,5 +1,10 @@
 %% model of objects vertical position in terms of time
 y0=0;
 v0=10;
-g=9.81;
-y= @(t)y0+v0*t+.5*g*t^2;
+g=9.81; % m/s^2
+y= @(t)y0+v0*t-.5*g*t.^2;
+t= linspace(0,2,100);
+figure ()
+fplot(y)
+xlabel ('time (s)'); ylabel ('Vertical position (m)'); title ('Position vs Time ')
+
