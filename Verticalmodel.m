e35d1bd258c982aea931baaee7064cdea80ff118
Rescuerich <richard.rojas.717@my.csun.edu>
2025-04-02 13:22:18 -0700

    Identify impact time, estimate velocity

--- Verticalmodel.m
+++ Verticalmodel.m
@@ -1,10 +1,24 @@
 %% model of objects vertical position in terms of time
-y0=0;
-v0=10;
+clear all, close all, clc 
+datadir= "C:\Users\Richard\Downloads\Clips-20250402T185746Z-001\Clips";
+filename="Throw_01.txt";
+fullPath = fullfile(datadir, filename);
+data =readmatrix(fullPath);
+tsampled=data(:,1);
+ysampled=data(:,3);
+i_impact =find (ysampled==min(ysampled)); %changed from i to i_impact cuz general practice 4 meaningful names
+t_impact = tsampled(i_impact);
+% previous code, arranges data
+y0=ysampled(1);
+v0=(ysampled(2)-ysampled(1))/(tsampled(2)-tsampled(1)); % Y component for the velocity, rise over run
+%%v0=10;commented out
 g=9.81; % m/s^2
 y= @(t)y0+v0*t-.5*g*t.^2;
-t= linspace(0,2,100);
+t= linspace(0,t_impact,100);
 figure ()
-fplot(y)
+%fplot(y,[0 3])
+plot(t,y(t));
 xlabel ('time (s)'); ylabel ('Vertical position (m)'); title ('Position vs Time ')
-
+hold on 
+plot(data(:,1),data(:,3));
+legend ('model','sample');
