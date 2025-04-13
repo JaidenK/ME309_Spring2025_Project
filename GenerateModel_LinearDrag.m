3d89f83d3f080afaf88602146b36723872651ece
JaidenK <king.jaiden@yahoo.com>
2025-04-12 18:56:23 -0700

    Add acceleration functions to model

--- GenerateModel_LinearDrag.m
+++ GenerateModel_LinearDrag.m
@@ -7,10 +7,10 @@ Model.Description = "Linear Drag Model";
 [g,r0,v0,b] = DecodeModelParams_FromVecToVar(ModelParameters);
 m=1;
 % implement equations that were derived analytically in the notes.
-% Model.acc_of_t.x = @(t) zeros(size(t));
-% Model.acc_of_t.y = @(t) -g*ones(size(t));
- Model.vel_of_t.x = @(t) v0(1)*exp(-(b*t)/m);
- Model.vel_of_t.y = @(t) (exp(-(b*t)/m)*(b*v0(2)+g*m)-g*m)/b;
+Model.acc_of_t.x = @(t) -(b*v0(1)/m)*exp(-(b*t)/m);
+Model.acc_of_t.y = @(t) -((b*v0(2)+g*m)/m)*exp(-(b*t)/m);
+Model.vel_of_t.x = @(t) v0(1)*exp(-(b*t)/m);
+Model.vel_of_t.y = @(t) (exp(-(b*t)/m)*(b*v0(2)+g*m)-g*m)/b;
 Model.pos_of_t.x = @(t) ((m*v0(1))/(b))*(1-exp(-b*t/m)) + r0(1); 
 Model.pos_of_t.y = @(t) (m/b)*(v0(2)+m*g/b)*(1-exp(-b*t/m))-(m*g*t/b) + r0(2);
 
