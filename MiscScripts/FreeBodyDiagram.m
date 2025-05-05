0ce23360aa745cec15746de11a6398b3eae3972f
JaidenK <king.jaiden@yahoo.com>
2025-05-04 19:55:17 -0700

    report updates and freebodydiagram.m

--- MiscScripts/FreeBodyDiagram.m
+++ MiscScripts/FreeBodyDiagram.m
@@ -1,21 +1,22 @@
-%clear all; close all; clc;
-
-
+clear all; close all; clc;
+drawAxes = false;
 
 figure('color','white');
+hold on;
 %set(groot, 'defaultAxesTickLabelInterpreter','latex'); set(groot, 'defaultLegendInterpreter','latex');
 
 % Draw axes
-axis_points = [1 0; 0 1; -1 0; 0 -1] * 5;
-quiver(zeros(size(axis_points(:,1))),...
-       zeros(size(axis_points(:,2))),...
-       axis_points(:,1),...
-       axis_points(:,2),'Color',[0.8 0.8 0.8]);
-hold on;
-text(axis_points(1,1),axis_points(1,2),"$+x$",'HorizontalAlignment','left','VerticalAlignment','middle','Interpreter','latex');
-text(axis_points(2,1),axis_points(2,2),"$+y$",'HorizontalAlignment','center','VerticalAlignment','bottom','Interpreter','latex');
-%text(axis_points(3,1),axis_points(3,2),"$-x$",'HorizontalAlignment','right','VerticalAlignment','middle','Interpreter','latex');
-%text(axis_points(4,1),axis_points(4,2),"$-y$",'HorizontalAlignment','center','VerticalAlignment','top','Interpreter','latex');
+if(drawAxes)
+    axis_points = [1 0; 0 1; -1 0; 0 -1] * 5;
+    quiver(zeros(size(axis_points(:,1))),...
+           zeros(size(axis_points(:,2))),...
+           axis_points(:,1),...
+           axis_points(:,2),'Color',[0.8 0.8 0.8]);
+    text(axis_points(1,1),axis_points(1,2),"$+x$",'HorizontalAlignment','left','VerticalAlignment','middle','Interpreter','latex');
+    text(axis_points(2,1),axis_points(2,2),"$+y$",'HorizontalAlignment','center','VerticalAlignment','bottom','Interpreter','latex');
+    %text(axis_points(3,1),axis_points(3,2),"$-x$",'HorizontalAlignment','right','VerticalAlignment','middle','Interpreter','latex');
+    %text(axis_points(4,1),axis_points(4,2),"$-y$",'HorizontalAlignment','center','VerticalAlignment','top','Interpreter','latex');
+end
 
 % Draw the point mass itself, located at the origin.
 scatter(0,0,120,'filled','MarkerEdgeColor',[117, 23, 40]/255, ...
@@ -26,15 +27,18 @@ set(gca,'Box','off');
 
 
 vec(1).forces = [0 -9.8];
-vec(1).text = {"$g$",sprintf("%g",norm(vec(1).forces))};
+%vec(1).text = {"$g$",sprintf("%g",norm(vec(1).forces))};
+vec(1).text = {"$g$"};
 vec(1).position = 1.2;
 
 vec(2).forces = [5 -5];
-vec(2).text = {"$v$",sprintf("%g",norm(vec(2).forces))};
+%vec(2).text = {"$v$",sprintf("%g",norm(vec(2).forces))};
+vec(2).text = {"$v$"};
 vec(2).position = 1.2;
 
 vec(3).forces = [-3 3];
-vec(3).text = {"$f_{drag}$",sprintf("%g",norm(vec(3).forces))};
+%vec(3).text = {"$f_{drag}$",sprintf("%g",norm(vec(3).forces))};
+vec(3).text = {"$f_{drag}$"};
 vec(3).position = 1.5;
 
 for i=1:length(vec)
